5e68f387449421079695a74c73fce1129a6e243e
vboxuser <bsanchezchavarria@gmail.com>
2026-07-06 06:51:48 +0000

    correcion de errores

--- include/componentes.h
+++ include/componentes.h
@@ -18,8 +18,8 @@ typedef struct
     char valor[MAX_TEXTO_COMPONENTE];
     int rotacion; //si es 0 sera horizontal, si es 1 vertical
 //agregado para permitir una conexion entre componentes
-    int nodo_inicio;
-    int nodo_fin;
+    //int nodo_inicio;
+    //int nodo_fin;
 }Resistor;
 
 //esto de aca ya no se toca (por ahora jeje creo que si para el snap y el seleccionado pero asi se ve bien)
@@ -43,10 +43,12 @@ void Mover_Resistor(ArregloResistores *punt_datos);
 void Rotar_Resistor(ArregloResistores *punt_datos);//permite la rotacion con posicion determinada
 
 //el siguiente void permite el mmalado para la conexcion de componentes con nodos. 
-void Conectar_Resistor(ArregloResistores *punt_datos, int indice_resistor, int nodo_inicio, int nodo_fin);
+
+//void Conectar_Resistor(ArregloResistores *punt_datos, int indice_resistor, int nodo_inicio, int nodo_fin);
 
 //el siguiente void permite el llamado para conectar con un cable el nodo y el componente
-void Dibujar_Conexiones_Resistores(ArregloResistores *resistores, ArregloNodos *nodos);
+
+//void Dibujar_Conexiones_Resistores(ArregloResistores *resistores, ArregloNodos *nodos);
 
 Rectangle Caja_de_seleccion_resistor(Resistor resistor); 
 
@@ -62,8 +64,8 @@ typedef struct
 
     int rotacion; //si es 0 sera horizontal, si es 1 vertical
 
-    int nodo_inicio;
-    int nodo_fin;
+    //int nodo_inicio;
+    //int nodo_fin;
 
 }Fuentes_T;
 
@@ -90,10 +92,12 @@ void Mover_Fuente_T(ArregloFuentes_T *punt_datos);
 void Rotar_Fuente_T(ArregloFuentes_T *punt_datos);//permite la rotacion con posicion determinada
 
 //el siguiente void permite la llamada  para la conexcion de componentes con nodos. 
-void Conectar_Fuente_T(ArregloFuentes_T *punt_datos, int indice_fuentes_T, int nodo_inicio, int nodo_fin);
+
+//void Conectar_Fuente_T(ArregloFuentes_T *punt_datos, int indice_fuentes_T, int nodo_inicio, int nodo_fin);
 
 //el siguiente void permite el llamado para conectar con un cable el nodo y el componente
-void Dibujar_Conexiones_Fuente_T(ArregloFuentes_T *fuentes_T, ArregloNodos *nodos);
+
+//void Dibujar_Conexiones_Fuente_T(ArregloFuentes_T *fuentes_T, ArregloNodos *nodos);
 
 Rectangle Caja_de_seleccion_Fuente_T(Fuentes_T fuente_T);
 //====== Para las Fuentes de Corriente (COMPONENTES) ======
@@ -107,8 +111,8 @@ typedef struct
     char valor[MAX_TEXTO_COMPONENTE];
 
     int rotacion; //si es 0 sera horizontal, si es 1 vertical
-    int nodo_inicio;
-    int nodo_fin;
+    //int nodo_inicio;
+    //int nodo_fin;
 }Fuentes_C;
 
 //esto de aca ya no se toca (por ahora jeje creo que si para el snap y el seleccionado pero asi se ve bien)
@@ -133,10 +137,12 @@ void Mover_Fuente_C(ArregloFuentes_C *punt_datos);
 void Rotar_Fuente_C(ArregloFuentes_C *punt_datos);//permite la rotacion con posicion determinada
 
 //el siguiente void permite la llamada  para la conexcion de componentes con nodos. 
-void Conectar_Fuente_C(ArregloFuentes_C *punt_datos, int indice_fuentes_C, int nodo_inicio, int nodo_fin);
+
+//void Conectar_Fuente_C(ArregloFuentes_C *punt_datos, int indice_fuentes_C, int nodo_inicio, int nodo_fin);
 
 //el siguiente void permite el llamado para conectar con un cable el nodo y el componente
-void Dibujar_Conexiones_Fuentes_C(ArregloFuentes_C *fuentes_C, ArregloNodos *nodos);
+
+//void Dibujar_Conexiones_Fuentes_C(ArregloFuentes_C *fuentes_C, ArregloNodos *nodos);
 
 Rectangle Caja_de_seleccion_Fuente_C(Fuentes_C fuente_T); 
 
